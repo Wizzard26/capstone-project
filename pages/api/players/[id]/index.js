cbe242f0ea48fe218777fbca2626a31a80b823b3
Rene van Dinter <info@gambit24.de>
2023-05-09 11:04:14 +0200

    edit function delete console logs

--- pages/api/players/[id]/index.js
+++ pages/api/players/[id]/index.js
@@ -7,9 +7,7 @@ export default async function handler(request, response) {
 
   if (request.method === "GET") {
     const player = await Player.findById(id);
-    console.log(player, id);
     if (!player) {
-
       return response.status(404).json({ status: 'Not found', message: `Player with ${id} not found.` })
     }
     return response.status(200).json(player)
